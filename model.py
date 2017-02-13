4b1a996341b0833aeab6ce1987d0e979a2e08450
MoonKang <mskang135@gmail.com>
2017-02-13 00:17:33 -0500

    Add files via upload

--- model.py
+++ model.py
@@ -16,13 +16,11 @@ from keras.optimizers import Adam
 from keras.utils import np_utils
 from keras.layers import Convolution2D, MaxPooling2D, Flatten, Input, ELU
 from keras import initializations
-from keras.models import load_model, model_from_json
-from keras.layers.normalization import BatchNormalization
 from sklearn.utils import shuffle
-from keras import backend as K
 import json
 import gc
 import math
+import pandas as pd
 
 
 # Referred https://chatbotslife.com/using-augmentation-to-mimic-human-driving-496b569760a9#.v76laaz7d
@@ -31,8 +29,6 @@ import math
 # In[19]:
 
 csv_path = 'data/driving_log.csv'  # udacity data (fastest graphic mode)
-new_size_col,new_size_row = 64, 64
-center_db, left_db, right_db, steer_db = [], [], [], []
 
 
 # In[20]:
@@ -42,6 +38,7 @@ center_db, left_db, right_db, steer_db = [], [], [], []
 np.random.seed(30)
 with open(csv_path) as csvfile:
     reader = csv.DictReader(csvfile)
+    center_db, left_db, right_db, steer_db = [], [], [], []
     for row in reader:
         if float(row['steering']) != 0.0:
             center_db.append(row['center'])
@@ -58,53 +55,32 @@ with open(csv_path) as csvfile:
                 steer_db.append(float(row['steering']))
 
 
-# In[21]:
-
-len(steer_db)
-
-
 # In[22]:
 
 center_db, left_db, right_db, steer_db = shuffle(center_db, left_db, right_db, steer_db)
 
 # split train & valid data
-img_train, img_valid, steer_train, steer_valid = train_test_split(center_db, steer_db, test_size=0.1, random_state=42)
-
-get_ipython().magic(u'matplotlib inline')
-plt.hist(steer_db, bins= 50)
-plt.xlabel('steering value')
-plt.ylabel('counts')
-# plt.show()
+img_train, img_valid, steer_train, steer_valid = train_test_split(center_db, steer_db, test_size=0.1, random_state=30)
 
 
-# In[23]:
-
-import pandas as pd
-x=pd.DataFrame(steer_db)
-x.describe()
-
-
-# In[24]:
-
-len(x[x[0]==0])
 
 
 # In[25]:
+new_size_col,new_size_row = 64, 64
 
 def random_brightness(image):
     """
-    randomly change brightness by converting Y value
+    randomly change brightness
     """
     hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    # brightness
+
     bv = .3 + np.random.random()
     hsv[::2] = hsv[::2]*bv
     return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
 
 def random_shift(image, steer):
     """
-    randomly shift image horizontally &
-    add proper steering angle to each image
+    randomly shift image horizontally
     """
     max_shift = 50
     max_ang = 0.2  # ang_per_pixel = 0.004
@@ -112,16 +88,18 @@ def random_shift(image, steer):
     rows, cols, _ = image.shape
 
     random_x = np.random.randint(-max_shift, max_shift + 1)
-    dst_steer = steer + (random_x / max_shift) * max_ang
-    if abs(dst_steer) > 1:
-        dst_steer = -1 if (dst_steer < 0) else 1
+    random_steer = steer + (random_x / max_shift) * max_ang
+    if abs(random_steer) > 1:
+        random_steer = -1 if (random_steer < 0) else 1
 
     mat = np.float32([[1, 0, random_x], [0, 1, 0]])
-    dst_img = cv2.warpAffine(image, mat, (cols, rows))
-    return dst_img, dst_steer
+    random_img = cv2.warpAffine(image, mat, (cols, rows))
+    return random_img, random_steer
 
 def random_shadow(image):
-    """generate random shadow in random region"""
+    """
+    generate random shadow in random region
+    """
     top_y = 320*np.random.uniform()
     top_x = 0
     bot_x = 160
@@ -145,14 +123,12 @@ def random_shadow(image):
 
 
 def random_flip(image, steering):
-    """ randomly flip image"""
+    """
+    randomly flip image
+    """
     flip_image, flip_steering = cv2.flip(image, 1), -steering
     return flip_image, flip_steering
 
-
-# In[26]:
-
-new_size_col,new_size_row = 64, 64
 
 def select_img(center_list, left_list, right_list, steer_list, num, offsets=0.22):
     """
@@ -173,16 +149,14 @@ def select_img(center_list, left_list, right_list, steer_list, num, offsets=0.22
 
 def crop_resize(image):
     """
-    Crop out top 1/5 of image and resize to 64x64
+    Crop out image and resize to 64x64
     """
-    shape = image.shape
-    # note: numpy arrays are (row, col)!
-    image = image[math.floor(shape[0]/5):shape[0]-25, 0:shape[1]]
-    image = cv2.resize(image,(new_size_col,new_size_row), interpolation=cv2.INTER_AREA)  
-    #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    #image = image/255.-.5
-    return image
-
+    cropped_img = image[63:136, 0:319]
+    resized_img = cv2.resize(cropped_img, (new_size_col, new_size_row), cv2.INTER_AREA)
+    img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB)
+    return img
+    
+    
 def generate_train(center_list, left_list, right_list, steer_list):
     num = np.random.randint(0, len(steer_list))
     image, steer = select_img(center_list, left_list, right_list, steer_list, num)
@@ -203,12 +177,12 @@ def generate_train(center_list, left_list, right_list, steer_list):
 
 # In[27]:
 
+
 def nvidia_model(dropout=.4):
     """
     architecture: 5 convolutional layer with dropout, 3 fully connected layer
     activation func : elu
     """
-
     model = Sequential()
     model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(new_size_row, new_size_col, 3), name='Normalized'))
     
@@ -217,8 +191,6 @@ def nvidia_model(dropout=.4):
     same, valid = ('same', 'valid')
     padding = [valid, valid, valid, valid, valid]
     strides = [(2, 2), (2, 2), (2, 2), (1, 1), (1, 1)]
-    
-    
     for l in range(len(nb_filters)):
         model.add(Convolution2D(nb_filters[l],
                                 kernel_size[l][0], kernel_size[l][1],
@@ -230,15 +202,12 @@ def nvidia_model(dropout=.4):
     neurons = [100, 50, 10]
     for l in range(len(neurons)):
         model.add(Dense(neurons[l], activation='elu'))
-        model.add(Dropout(dropout))
-    
+        model.add(Dropout(dropout))   
     model.add(Dense(1, activation='elu', name='Out'))
     model.add(Dense(1))
     model.summary()
     return model
 
-
-# In[29]:
 
 def model2():
     """
@@ -249,14 +218,27 @@ def model2():
     model = Sequential()
     model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(new_size_row, new_size_col, 3), name='Normalized'))
     
+    nb_filters = [32, 64, 128, 128]
+    kernel_size = [(3, 3), (3, 3), (3, 3), (2, 2)]
+    same, valid = ('same', 'valid')
+    padding = [same, same, same, same]
+    strides = [(2, 2), (2, 2), (1, 1), (1, 1)]
     
-    model.add(Convolution2D(32, 3, 3, border_mode='same', subsample=(2, 2), activation='relu', name='Conv1'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='same'))
-    model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(2, 2), activation='relu', name='Conv2'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='same'))
-    model.add(Convolution2D(128, 3, 3, border_mode='same', subsample=(1, 1), activation='relu', name='Conv3'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='same'))
-    model.add(Convolution2D(128, 2, 2, border_mode='same', subsample=(1, 1), activation='relu', name='Conv4'))
+    a= len(nb_filters)-1
+    for l in range(len(nb_filters)-1):
+        model.add(Convolution2D(nb_filters[l],
+                                kernel_size[l][0], kernel_size[l][1],
+                                border_mode=padding[l],
+                                subsample=strides[l],
+                                activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='same'))
+    
+    
+    model.add(Convolution2D(nb_filters[a],
+                                kernel_size[a][0], kernel_size[a][1],
+                                border_mode=padding[a],
+                                subsample=strides[a],
+                                activation='relu'))
     model.add(Flatten())
     model.add(Dropout(0.5))
     model.add(Dense(128, activation='relu', name='FC1'))
@@ -273,38 +255,50 @@ def model2():
 
 def model3():
     """
-    architecture: 4 convolutional layer, 3 fully connected layer with dropout
+    architecture: 4 convolutional layer with dropout, 3 fully connected layer 
     activation func : relu
     pooling : maxpooling
     """
-    model = Sequential()
+        model = Sequential()
     model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(new_size_row, new_size_col, 3), name='Normalized'))
     
+    nb_filters = [32, 64, 128, 128]
+    kernel_size = [(3, 3), (3, 3), (3, 3), (2, 2)]
+    same, valid = ('same', 'valid')
+    padding = [same, same, same, same]
+    strides = [(2, 2), (2, 2), (1, 1), (1, 1)]
     
-    model.add(Convolution2D(32, 3, 3, border_mode='same', subsample=(2, 2), activation='relu', name='Conv1'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='same'))
-    model.add(Dropout(0.4))
-    model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(2, 2), activation='relu', name='Conv2'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='same'))
-    model.add(Dropout(0.4))
-    model.add(Convolution2D(128, 3, 3, border_mode='same', subsample=(1, 1), activation='relu', name='Conv3'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='same'))
-    model.add(Dropout(0.4))
-    model.add(Convolution2D(128, 2, 2, border_mode='same', subsample=(1, 1), activation='relu', name='Conv4'))
+    a= len(nb_filters)-1
+    for l in range(len(nb_filters)-1):
+        model.add(Convolution2D(nb_filters[l],
+                                kernel_size[l][0], kernel_size[l][1],
+                                border_mode=padding[l],
+                                subsample=strides[l],
+                                activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='same'))
+        model.add(Dropout(0.5))
+    
+    model.add(Convolution2D(nb_filters[a],
+                                kernel_size[a][0], kernel_size[a][1],
+                                border_mode=padding[a],
+                                subsample=strides[a],
+                                activation='relu'))
     model.add(Flatten())
+    model.add(Dropout(0.5))
     model.add(Dense(128, activation='relu', name='FC1'))
+    model.add(Dropout(0.5))
     model.add(Dense(128, activation='relu', name='FC2'))
+    model.add(Dropout(0.5))
     model.add(Dense(64, activation='relu', name='FC3'))
     model.add(Dense(1))
     model.summary()
     return model
 
 
+
 # # Train
 
 # In[31]:
-
-new_size_col,new_size_row = 64, 64
 log_path = './data'
 
 def valid_img(img_valid, steer_valid, num):
@@ -348,7 +342,7 @@ def generate_train_batch(center, left, right, steering, batch_size):
         yield image_set, steering_set
 
 
-# In[ ]:
+
 
 batch_size = 128
 epoch = 14
@@ -357,38 +351,19 @@ train_generator = generate_train_batch(center_db, left_db, right_db, steer_db, b
 image_val, steer_val = generate_valid(img_valid, steer_valid)
 
 
-# In[ ]:
-
 
 model = model2()
 
-adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+adam = Adam(lr=1e-4)
 model.compile(optimizer=adam, loss='mse')
 
-model_json = 'model_5.json'
 model_weights = 'model_5.h5'
 
 history = model.fit_generator(train_generator, samples_per_epoch=20224, nb_epoch=epoch,
                               validation_data=(image_val, steer_val), verbose=1)
 
-json_string = model.to_json()
 
-try:
-    os.remove(model_json)
-    os.remove(model_weights)
-except OSError:
-    pass
-
-with open(model_json, 'w') as jfile:
-    json.dump(json_string, jfile)
 model.save(model_weights)
-
-# to avoid " 'NoneType' object has no attribute 'TF_DeleteStatus' " error
-gc.collect()
-K.clear_session()
-
-
-# In[ ]:
 
 
 
